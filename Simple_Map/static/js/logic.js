d0156436e2da35224131ec4611d8ca041d1a763d
Olivia Snyder <oliviasny2@gmail.com>
2022-05-28 18:52:41 -0400

    update

--- Simple_Map/static/js/logic.js
+++ Simple_Map/static/js/logic.js
@@ -1,3 +1,14 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:14df622b0d673fb167c7dab16947681c1ce918e70ea0ab56baf5a5b74cb9e65c
-size 755
+// Add console.log to check to see if our code is working.
+console.log("working");
+
+// Create the map object with a center and zoom level.
+let map = L.map('mapid').setView([40.7, -94.5], 4);
+
+// We create the tile layer that will be the background of our map.
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY
+});
+// Then we add our 'graymap' tile layer to the map.
+streets.addTo(map);
